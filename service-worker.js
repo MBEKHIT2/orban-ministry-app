f03b5373799b8ab890429155188ff2064c88cd9b
MBEKHIT2 <bekhitmike@gmail.com>
2026-01-12 11:46:16 -0500

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open('orban-cache-v1').then(cache => {
+  event.waitUntil(caches.open('orban-cache-v2').then(cache => {
     return cache.addAll([
-      './smsv_orban_ministry.html',
+      './index.html',
       './manifest.webmanifest',
       './orban.png'
     ]);
   }));
 });
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
+``
